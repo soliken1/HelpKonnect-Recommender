ef901f220c613715e5fdb8e57690500fa766e297
Soliken <kennethrex456@gmail.com>
2024-10-29 08:14:50 +0800

    Update CORS

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # main.py
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import openai
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
